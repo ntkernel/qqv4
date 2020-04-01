7b1000dc9f4d04c1641968dfc4fa020743a5dc86
Your Name <you@example.com>
2020-04-01 11:22:16 +0800

    init

---
+++ cs/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using llt.qq;
+namespace QQ_LOCAL
+{
+    class Program
+    {
+        static HttpListener LIS;
+        static LCBOT bot;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[Main]:program started");
+            Console.WriteLine("[Main]:starting Mirai Bot");
+            bot = new LCBOT();
+            bot.init(1234567890, "123456", "C:\\apidb\\QQ","http://localhost/qqv3/x-local-recv");
+            Console.WriteLine("[Main]:Mirai Bot started");
+            Console.WriteLine("[Main]:starting Http Listener");
+            LIS = new HttpListener();
+            LIS.AuthenticationSchemes = AuthenticationSchemes.Anonymous;
+            LIS.Prefixes.Add("http://+:4888/X_LLT_QQ/v3/");
+            LIS.Start();
+            Console.WriteLine("[Main]:Http Listener started at" + LIS.Prefixes);
+            Task.Run(() =>
+            {
+                Listen();
+            });
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+        static void Listen()
+        {
+            while (true)
+            {
+                HttpListenerContext ctx = LIS.GetContext();
+                StreamWriter writer = new StreamWriter(ctx.Response.OutputStream);
+                StreamReader reader = new StreamReader(ctx.Request.InputStream);
+                string inStr;
+                if (!ctx.Request.IsLocal)
+                {
+                    Console.WriteLine("[HTTP]:recv unauth request");
+                    writer.Write("UNAUTH REQUEST");
+                    writer.Close();
+                    ctx.Response.OutputStream.Close();
+                }
+                else
+                {
+                    inStr = reader.ReadToEnd();
+                    string action = ctx.Request.QueryString["action"];
+                    string id = ctx.Request.QueryString["id"];
+                    if (action == "sendfriendmsg")
+                    {
+                        Console.WriteLine("[HTTP]:recv send friend message request");
+                        //string id
+                        try
+                        {
+                            Console.WriteLine("[Bot]:send friend request");
+                            Task.Run(() =>
+                            {
+                                bot.sendFriendMsg(long.Parse(id), inStr);
+                            });
+                            Console.WriteLine("[Bot]:sent friend request");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("[Bot]:fail to send friend request");
+                        }
+                        writer.Write("Friend request");
+                        writer.Close();
+                        ctx.Response.OutputStream.Close();
+                    }
+                    else
+                    {
+                        if (action == "sendgroupmsg")
+                        {
+                            Console.WriteLine("[HTTP]:recv send group message request");
+                            try
+                            {
+                                Console.WriteLine("[Bot]:send group request");
+                                Task.Run(() =>
+                                {
+
+                                    bot.sendGroupMsg(long.Parse(id), inStr);
+                                });
+                                Console.WriteLine("[Bot]:sent group request");
+                            }
+                            catch
+                            {
+                                Console.WriteLine("[Bot]:fail to send group request");
+                            }
+                            writer.Write("Group request");
+                            writer.Close();
+                            ctx.Response.OutputStream.Close();
+                        }
+                        else
+                        {
+                            if (action == "reset")
+                            {
+                                Process.Start(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                                Process.GetCurrentProcess().Kill();
+                            }
+                            Console.WriteLine("recv wrong request");
+                            writer.Write("Wrong request");
+                            writer.Close();
+                            ctx.Response.OutputStream.Close();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ kt/src/main/kotlin/llt/qq/LCBOT.kt
@@ -0,0 +1,197 @@
+package llt.qq
+
+
+
+import net.mamoe.mirai.alsoLogin
+import net.mamoe.mirai.contact.sendMessage
+import net.mamoe.mirai.qqandroid.Bot
+import net.mamoe.mirai.qqandroid.QQAndroid
+import kotlinx.coroutines.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import net.mamoe.mirai.Bot
+import net.mamoe.mirai.event.events.BotOfflineEvent
+import net.mamoe.mirai.event.subscribeAlways
+import net.mamoe.mirai.message.FriendMessage
+import net.mamoe.mirai.message.GroupMessage
+import net.mamoe.mirai.message.data.Image
+import net.mamoe.mirai.message.data.Message
+import net.mamoe.mirai.message.data.MessageChain
+import net.mamoe.mirai.message.data.PlainText
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import java.io.File
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.system.exitProcess
+
+
+class LCBOT{
+    private var cProcess: Int = 0
+    private var path: String = ""
+    private var uri: String = ""
+    var bot: Bot? = null
+    fun init(id:Long, pwd:String, path:String, uri:String){
+        //bot = QQAndroid.Bot(id,pwd){randomDeviceName=false}.alsoLogin()
+        this.path = path
+        this.uri = uri
+        cProcess=1
+        GlobalScope.launch {
+            bot = QQAndroid.Bot(id,pwd){}.alsoLogin()
+            //bot =
+            cProcess=0
+        }
+        while (cProcess==1){
+            Thread.sleep(100)
+        }
+        bot?.subscribeAlways<FriendMessage> {
+            saveMsg(message,false, sender.id)
+            println(message)
+
+            //message[][0]
+            post(uri+"?action=friend&id="+sender.id,message.toString())
+            println("[KTMT]:Friendmsg "+uri+"?action=friend&id="+sender.id+" "+message.toString())
+        }
+        bot?.subscribeAlways<GroupMessage>{
+            saveMsg(message,true, group.id)
+            println(message)
+            post(uri+"?action=group&id="+group.id,message.toString())
+            println("[KTMT]:Groupmsg "+uri+"?action=group&id="+group.id+" "+message.toString())
+        }
+        bot?.subscribeAlways<BotOfflineEvent.Force>{
+            println("offline")
+            post("$uri?action=reset&id=4888","RESET")
+        }
+    }
+    fun sendFriendMsg(id:Long, msg:String){
+        cProcess=1
+        GlobalScope.launch {
+            try{
+                bot!!.getFriend(id).sendMessage(msg)
+                saveSentMsg(id,false,msg)
+            }catch (e:Exception){
+                //todo
+            }
+            cProcess=0
+        }
+        while (cProcess==1){
+            Thread.sleep(100)
+        }
+    }
+    fun sendGroupMsg(id:Long,msg:String){
+        cProcess=1
+        GlobalScope.launch {
+            try{
+                bot!!.getGroup(id).sendMessage(msg)
+                saveSentMsg(id,true,msg)
+            }catch (E:Exception){
+                //todo
+            }
+            cProcess=0
+        }
+        while (cProcess==1){
+            Thread.sleep(100)
+        }
+    }
+    @Suppress("SimpleDateFormat")
+    private fun saveSentMsg(id: Long,isGroup: Boolean,content: String){
+        var cT:String = SimpleDateFormat("yyyyMMddHHmmssSSS").format(Date())
+        var iPath:String = path
+        if(!iPath.endsWith("\\")){
+            iPath += "\\"
+        }
+        if(isGroup){
+            iPath += "Group\\"
+        }else{
+            iPath += "Friend\\"
+        }
+        iPath = iPath + id + "\\"
+        iPath += cT
+        iPath += "\\"
+        File(iPath).mkdirs()
+        iPath += "1TEXT"
+        File(iPath).writeText(content)
+    }
+    @Suppress("SimpleDateFormat")
+    private fun saveMsg(msg: MessageChain, isGroup: Boolean, id: Long){
+        var cC:Int = 1
+        var cT:String = SimpleDateFormat("yyyyMMddHHmmssSSS").format(Date())
+        var iPath:String = path
+        if(!iPath.endsWith("\\")){
+            iPath += "\\"
+        }
+        if(isGroup){
+            iPath += "Group\\"
+        }else{
+            iPath += "Friend\\"
+        }
+        iPath = iPath + id + "\\"
+        iPath += cT
+        iPath += "\\"
+        //C:\example\Friend\cT\
+        var cType: String
+        msg.forEach{
+            cType = "UR"
+            if(it is PlainText){
+                cType = "TEXT"
+            }
+            if(it is Image){
+                cType = "IMAGE"
+            }
+            var pPath:String = iPath
+            File(pPath).mkdirs()
+            File(iPath + cC + cType).writeText(it.toString())
+
+            cC++
+        }
+        /*
+        while(cC<msg.size){
+            //var cType
+            if(msg[cC] is PlainText){
+                cType = "TEXT"
+            }
+            if(msg[cC] is Image){
+                cType = "IMAGE"
+            }
+            var pPath:String = iPath
+            File(pPath).mkdirs()
+            File(iPath + cC + cType).writeText(msg[cC].toString())
+
+            cC++
+        }*/
+    }
+    private fun post(uri:String,content:String): String? {
+        try{
+            var cli = OkHttpClient()
+            val requestBody:RequestBody = content.toRequestBody()
+            var request:Request = Request.Builder().url(uri).post(requestBody).build()
+            var rsp:Response=cli.newCall(request).execute()
+            return rsp.body?.string()
+        }catch (ex:Exception){
+            println("==========error")
+            println(uri)
+            println(content)
+            println(ex.toString())
+            return ex.toString()
+        }
+    }
+}
+
+
+
+private fun posts(uri:String,content:String): String? {
+    try{
+        var cli = OkHttpClient()
+        val requestBody:RequestBody = content.toRequestBody()
+        var request:Request = Request.Builder().url(uri).post(requestBody).build()
+        var rsp:Response=cli.newCall(request).execute()
+        return rsp.body?.string()
+    }catch (ex:Exception){
+        return ex.toString()
+    }
+}
