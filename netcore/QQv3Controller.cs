37dbc392c41cc1b6f7fdfc863d2c20159cca5ba1
Your Name <you@example.com>
2020-04-01 14:28:19 +0800

    add server

---
+++ netcore/QQv3Controller.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LLTAPI_v3.Modules;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace LLTAPI_v3.Controllers
+{
+    [Route("qqv3")]
+    [ApiController]
+    public class QQv3Controller : ControllerBase
+    {
+        v3Auth vA = new v3Auth();
+        QQv3R vR = new QQv3R();
+        WX_T wxt = new WX_T();
+        Settings settings = new Settings();
+        [Route("sendfriendmsg")]
+        [HttpPost]
+        public string sendfriendmsg([FromQuery]long id,[FromQuery]string token)
+        {
+            
+            StreamReader reader = new StreamReader(Request.Body);
+            vR.sendFriendMsg(id, reader.ReadToEnd());
+            return "sent";
+        }
+        [Route("sendgroupmsg")]
+        [HttpPost]
+        public string sendgroupmsg([FromQuery]long id, [FromQuery]string token)
+        {
+            //TODO
+            if (!vA.checkToken(token))
+            {
+                return "-1";
+            }
+            StreamReader reader = new StreamReader(Request.Body);
+            vR.sendGroupMsg(id, reader.ReadToEnd());
+            return "sent";
+        }
+        [Route("forcereset")]
+        [HttpGet]
+        public string forcereset([FromQuery]string token)
+        {
+            
+            vR.reset();
+            return "ok";
+        }
+        [Route("x-local-recv")]
+        [HttpPost]
+        public string localconnect([FromQuery]long id,[FromQuery]string action)
+        {
+            StreamReader reader = new StreamReader(Request.Body);
+            string instr = reader.ReadToEnd();
+            if (action == "friend")
+            {
+                //TODO
+                
+            }
+            if (action == "group")
+            {
+                //TODO
+            }
+            if(action == "reset" && id == 4888 && instr == "RESET" && authDB.resetable)
+            {
+                vR.reset();
+            }
+            return "X";
+        }
+    }
+}
---
+++ netcore/QQv3R.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json.Linq;
+namespace LLTAPI_v3.Modules
+{
+    public class QQv3R
+    {
+        REST rest = new REST();
+        Settings settings = new Settings();
+        public void sendFriendMsg(long id,string content)
+        {
+            rest.g_strHttpPostOrPutRequest("POST", "http://localhost:4888/x_llt_qq/v3?action=sendfriendmsg&id=" + id, content);
+        }
+        public void sendGroupMsg(long id, string content)
+        {
+            rest.g_strHttpPostOrPutRequest("POST", "http://localhost:4888/x_llt_qq/v3?action=sendgroupmsg&id=" + id, content);
+        }
+        public void reset()
+        {
+            rest.g_strHttpPostOrPutRequest("POST", "http://localhost:4888/x_llt_qq/v3?action=reset&id=0", "x");
+        }
+        public string getFriendChatList()
+        {
+            string[] ChatL = Directory.GetDirectories(settings.v3FDir);
+            int Dlc = ChatL.Length;
+            JArray ja = new JArray();
+            int cC = 0;
+            while (cC < Dlc)
+            {
+                ja.Add(ChatL[cC].Substring(settings.v3FDir.Length));
+                cC++;
+            }
+            return ja.ToString();
+        }
+        public string getGroupChatList()
+        {
+            string[] ChatL = Directory.GetDirectories(settings.v3GDir);
+            int Dlc = ChatL.Length;
+            JArray ja = new JArray();
+            int cC = 0;
+            while (cC < Dlc)
+            {
+                ja.Add(ChatL[cC].Substring(settings.v3GDir.Length));
+                cC++;
+            }
+            return ja.ToString();
+        }
+        public string getChat(long id,bool isGroup)
+        {
+            string iPath = "";
+            dynamic Joperator;
+            if (isGroup)
+            {
+                iPath = settings.v3GDir;
+            }
+            else
+            {
+                iPath = settings.v3FDir;
+            }
+            iPath += id + "\\";
+            if (!Directory.Exists(iPath))
+            {
+                return "WrongRQ";
+            }
+            JArray retJsonArr = new JArray();//level 0
+            //get All Dir
+            string[] perSentMsgList = Directory.GetDirectories(iPath);
+            long cC1 = 0;//level 1
+            while (cC1 < perSentMsgList.Length)
+            {
+                string lv1Path = perSentMsgList[cC1] + "\\";
+                long cC2 = 0;//enter time dir and find all message such as 1TEXT
+                JArray perMsgJsonList = new JArray();//level 1
+                string[] childMsgList = Directory.GetFiles(lv1Path);
+                while (cC2 < childMsgList.Length)
+                {
+                    string childMsgPath = childMsgList[cC2];
+                    string childMsgName = childMsgPath.Substring(lv1Path.Length);
+                    string type = "";
+                    string childMsgId = "";
+                    if (childMsgName.EndsWith("TEXT"))
+                    {
+                        type = "TEXT";
+                        childMsgId = childMsgName.Substring(0, childMsgName.Length - 4);
+                    }
+                    if (childMsgName.EndsWith("IMAGE"))
+                    {
+                        type = "IMAGE";
+                        childMsgId = childMsgName.Substring(0, childMsgName.Length - 5);
+                    }
+                    string msgContent = File.ReadAllText(childMsgPath);
+                    JToken childMsgJson = new JObject();//level 2
+                    Joperator = childMsgJson;
+                    Joperator.type = type;
+                    Joperator.content = msgContent;
+                    perMsgJsonList.Add(childMsgJson);
+                    cC2++;
+                }
+                retJsonArr.Add(perMsgJsonList);
+                cC1++;
+            }
+            return retJsonArr.ToString();
+        }
+    }
+}
